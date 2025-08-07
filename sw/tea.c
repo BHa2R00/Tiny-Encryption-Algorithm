677653ddf778dfa3e3bce573ea3db175b8a05bd1
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2025-08-07 06:59:41 +0000

    Update tea.c

--- sw/tea.c
+++ sw/tea.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#define SHIFT 3
 void tea_enc (uint16_t* dat, uint16_t* key) {
   uint16_t x = dat[0];
   uint16_t y = dat[1];
   uint16_t delta = 0x9E37;
   uint16_t sum = 0;
   int i;
-  for(i=0;i<(1<<5);i++){
+  for(i=0;i<(1<<SHIFT);i++){
     sum += delta;
     x += ((y << 4) + key[0]) ^ (y + sum) ^ ((y >> 5) + key[1]);
     y += ((x << 4) + key[2]) ^ (x + sum) ^ ((x >> 5) + key[3]);
@@ -19,9 +20,9 @@ void tea_dec (uint16_t* dat, uint16_t* key) {
   uint16_t x = dat[0];
   uint16_t y = dat[1];
   uint16_t delta = 0x9E37;
-  uint16_t sum = delta << 5;
+  uint16_t sum = delta << SHIFT;
   int i;
-  for(i=0;i<(1<<5);i++){
+  for(i=0;i<(1<<SHIFT);i++){
     y -= ((x << 4) + key[2]) ^ (x + sum) ^ ((x >> 5) + key[3]);
     x -= ((y << 4) + key[0]) ^ (y + sum) ^ ((y >> 5) + key[1]);
     sum -= delta;
